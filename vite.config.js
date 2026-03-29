39633f036aa1e8b58cb0c6f332319a4df612b7bd
Tokitoto77 <st.ritz.530@gmail.com>
2026-03-29 17:25:43 +0900

    Prepare GitHub Pages recovery site

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/portfolio-lp/',
+    base: '/demoa-works-portfolio/',
     build: {
         rollupOptions: {
             input: {
